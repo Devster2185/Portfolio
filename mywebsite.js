cedae1fcceac8211417d8f3e3d1a96d8ffdef225
Devster <108146717+Devster2185@users.noreply.github.com>
2025-06-04 11:10:47 -0500

    Create mywebsite.js

---
+++ mywebsite.js
@@ -0,0 +1,20 @@
+alert('Welcome to my site!')
+
+
+function showGreenContent() {
+    document.getElementById('green-content').textContent =
+    'Italian , Portuguese , English and Spanish'
+};
+
+
+function showOrangeContent() {
+    document.getElementById('orange-content').textContent =
+    'HTML , CSS and JavaScript'
+};
+
+
+function showBlueContent() {
+    document.getElementById('blue-content').textContent = 
+    'Instagram and Tiktok : @vhprod21 '
+}
+
